ca89e4f436fd887bf6c905276dcc14e3dd23c6c7
Peter Mack <101904765+Jgfyyvzss@users.noreply.github.com>
2024-05-09 17:00:15 +1000

    Run every boot on read-only FS

--- WittyPi/beforeScript.sh
+++ WittyPi/beforeScript.sh
@@ -7,7 +7,8 @@
 # Remarks: please use absolute path of the command, or it can not be found (by root user).
 # Remarks: you may append '&' at the end of command to avoid blocking the main daemon.sh.
 #
-lastupdate=$(/usr/bin/date -r schedule.wpi +"%m.wpi")
-thismonth=$(/usr/bin/date "+%m.wpi")
-[ $lastupdate != $thismonth ] \
-&& /usr/bin/cp -f "$thismonth" schedule.wpi
+# lastupdate=$(/usr/bin/date -r schedule.wpi +"%m.wpi") # No use on read only OverlayFS
+thismonth=$(/usr/bin/date "+%m.wpi") 
+# [ $lastupdate != $thismonth ] \
+# && /usr/bin/cp -f "$thismonth" schedule.wpi
+/usr/bin/cp -f "$thismonth" /wittyPi/schedule.wpi #this line only on OverlayFS system.
